07ffd7d5543c54671ced1b2431283f4853e75634
Tanja Hennis <tmhennis@gmail.com>
2017-01-14 21:01:14 +0100

    update seeds and factories

--- spec/factories/details.rb
+++ spec/factories/details.rb
@@ -16,27 +16,18 @@ FactoryGirl.define do
     indication_dislocation { Faker::Boolean.boolean }
     indication_stemfracture { Faker::Boolean.boolean }
     indication_other { Faker::Lorem.word }
-    approach_posterolateral { Faker::Lorem.word }
-    approach_hardinge { Faker::Lorem.word }
-    approach_anterolateral { Faker::Lorem.word }
-    approach_anterior { Faker::Lorem.word }
+    approach { Faker::Lorem.word }
     approach_brand { Faker::Lorem.word }
-    approach_cemented { Faker::Boolean.boolean }
-    approach_cemented_loose { Faker::Lorem.word }
-    approach_cemented_wellfixed { Faker::Lorem.word }
+    approach_cemented { Faker::Lorem.word }
+    approach_cemented { Faker::Lorem.word }
     approach_uncemented_screws { Faker::Lorem.word }
     approach_cupsize { Faker::Lorem.word }
-    liner_poly { Faker::Lorem.word }
-    liner_ceramic { Faker::Lorem.word }
-    liner_metal { Faker::Lorem.word }
-    acetabularbone_good { Faker::Boolean.boolean }
-    acetabularbone_cysts { Faker::Boolean.boolean }
-    acetabularbone_migration { Faker::Boolean.boolean }
+    liner { Faker::Lorem.word }
+    acetabular_bone_quality { Faker::Boolean.boolean }
     paproskygrade { Faker::Lorem.word }
-    pelvic_discontinuity_string { Faker::Lorem.word }
+    pelvic_discontinuity { Faker::Lorem.word }
     stem_cemented { Faker::Lorem.word }
-    stem_fixation_wellfixed { Faker::Lorem.word }
-    stem_fixation_loose { Faker::Lorem.word }
+    stem_fixation { Faker::Lorem.word }
     stem_brand { Faker::Lorem.word }
     stem_size { Faker::Lorem.word }
     stem_length { Faker::Number.decimal(2) }
--- spec/factories/patients.rb
+++ spec/factories/patients.rb
@@ -4,18 +4,18 @@ FactoryGirl.define do
     comorbidities { Faker::Lorem.words(5) }
     medication { Faker::Lorem.words(7) }
     allergies { Faker::Lorem.words(4) }
-    postop_VTE_prophylaxis { Faker::Lorem.words(5) }
-    representation { Faker::Lorem.sentence }
-    fluoroscopy { Faker::Lorem.word }
     cell_saver { Faker::Boolean.boolean }
     hdu_bed { Faker::Boolean.boolean }
     catheter { Faker::Boolean.boolean }
     central_line_needed { Faker::Boolean.boolean }
+    postop_VTE_prophylaxis { Faker::Lorem.words(5) }
     infection_micro_organism { Faker::Lorem.words(3) }
     infection_antibiotics { Faker::Lorem.words(3) }
     most_recent_crp { Faker::Lorem.words(3) }
     five_samples_microbiology { Faker::Boolean.boolean }
     frozen_sections { Faker::Boolean.boolean }
     excise_scar { Faker::Boolean.boolean }
+    representation { Faker::Lorem.sentence }
+    fluoroscopy { Faker::Lorem.word }
   end
 end
