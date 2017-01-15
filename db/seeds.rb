2f49db7230de23c0e5a741d5ec256bd25bd9c03c
Tanja Hennis <tmhennis@gmail.com>
2017-01-15 16:05:17 +0100

    cup implant factory and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -108,3 +108,26 @@ stem_removal_instruments = []
     broken_stem_removal_set: Faker::Boolean.boolean
   )
 end
+
+# Cup implant instruments
+cup_implant_instruments = []
+5.times do
+  cup_implant_instruments << CupImplantInstrument.create(
+    press_fit: Faker::Lorem.word,
+    press_fit_other: Faker::Lorem.word,
+    type_cup: Faker::Lorem.word,
+    cup_brand: Faker::Boolean.boolean,
+    cup_size: Faker::Boolean.boolean,
+    cup_liner: Faker::Boolean.boolean,
+    screws_drill: Faker::Boolean.boolean,
+    femoral_head: Faker::Boolean.boolean,
+    impaction_bg_instruments: Faker::Boolean.boolean,
+    bone_mill: Faker::Boolean.boolean,
+    jamshidi_needle: Faker::Boolean.boolean,
+    mesh: Faker::Boolean.boolean,
+    tm_wedges: Faker::Boolean.boolean,
+    plad: Faker::Boolean.boolean,
+    cupcage: Faker::Boolean.boolean,
+    custom_cage: Faker::Boolean.boolean
+  )
+end
--- spec/factories/cup_implant_instruments.rb
+++ spec/factories/cup_implant_instruments.rb
@@ -1,20 +1,20 @@
 FactoryGirl.define do
   factory :cup_implant_instrument do
-    press_fit "MyString"
-    press_fit_other "MyString"
-    type_cup "MyString"
-    cup_brand false
-    cup_size false
-    cup_liner false
-    screws_drill false
-    femoral_head false
-    impaction_bg_instruments false
-    bone_mill false
-    jamshidi_needle false
-    mesh false
-    tm_wedges ""
-    plad false
-    cupcage false
-    custom_cage false
+    press_fit { Faker::Lorem.word }
+    press_fit_other { Faker::Lorem.word }
+    type_cup { Faker::Lorem.word }
+    cup_brand { Faker::Boolean.boolean }
+    cup_size { Faker::Boolean.boolean }
+    cup_liner { Faker::Boolean.boolean }
+    screws_drill { Faker::Boolean.boolean }
+    femoral_head { Faker::Boolean.boolean }
+    impaction_bg_instruments { Faker::Boolean.boolean }
+    bone_mill { Faker::Boolean.boolean }
+    jamshidi_needle { Faker::Boolean.boolean }
+    mesh { Faker::Boolean.boolean }
+    tm_wedges { Faker::Boolean.boolean }
+    plad { Faker::Boolean.boolean }
+    cupcage { Faker::Boolean.boolean }
+    custom_cage { Faker::Boolean.boolean }
   end
 end
