50ccb1f37770766a7249f7f9d81dcbbb747dca0b
Tanja Hennis <tmhennis@gmail.com>
2017-01-15 17:00:24 +0100

    stem implant seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -131,3 +131,24 @@ cup_implant_instruments = []
     custom_cage: Faker::Boolean.boolean
   )
 end
+
+# Stem implant instruments
+stem_implant_instruments = []
+5.times do
+  stem_implant_instruments << StemImplantInstrument.create(
+    aim_for_fit: Faker::Lorem.word,
+    type_stem: Faker::Lorem.word,
+    stem_brand: Faker::Lorem.word,
+    stem_size: Faker::Lorem.word,
+    stem_offset: Faker::Lorem.word,
+    cement_type: Faker::Lorem.word,
+    antibiotics: Faker::Lorem.word,
+    cement_plug: Faker::Boolean.boolean,
+    head: Faker::Lorem.word,
+    head_brand: Faker::Lorem.word,
+    sleeve_adapter: Faker::Boolean.boolean,
+    dall_milles_wires: Faker::Boolean.boolean,
+    trochanteric_plate: Faker::Boolean.boolean,
+    gentamycin_sponges: Faker::Boolean.boolean
+  )
+end
