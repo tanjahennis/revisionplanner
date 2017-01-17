0dae0bfb4fde19e0c8e440f666855f210c6c22a4
Tanja Hennis <tmhennis@gmail.com>
2017-01-17 17:08:36 +0100

    profile model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170115153945) do
+ActiveRecord::Schema.define(version: 20170117160702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,16 @@ ActiveRecord::Schema.define(version: 20170115153945) do
     t.datetime "updated_at",                null: false
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "role"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
+  end
+
   create_table "stem_implant_instruments", force: :cascade do |t|
     t.string   "aim_for_fit"
     t.string   "stem_cemented"
@@ -178,4 +188,5 @@ ActiveRecord::Schema.define(version: 20170115153945) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "profiles", "users"
 end
---
+++ spec/factories/profiles.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :profile do
+    first_name "MyString"
+    last_name "MyString"
+    role "MyString"
+    user nil
+  end
+end
