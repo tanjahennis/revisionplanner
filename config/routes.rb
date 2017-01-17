346b9bca45c5366f611029ea07c42cff89f5b68b
Tanja Hennis <tmhennis@gmail.com>
2017-01-17 19:16:35 +0100

    fix edit profile navbar

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :profiles, only: [:new, :edit, :create, :update]
+  resources :users
   resources :patients
   resources :details
   resources :cup_removals
