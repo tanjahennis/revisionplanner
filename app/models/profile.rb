52d54581f20ff4e4273dd3eddeb339065db1a632
Tanja Hennis <tmhennis@gmail.com>
2017-01-17 17:24:36 +0100

    profile validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,11 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :role, presence: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
